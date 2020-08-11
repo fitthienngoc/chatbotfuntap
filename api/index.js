cd0203abb1e6f08568b9b984ef4ea342f384df1a
Thien Ngoc <ngocnt@funtap.vn>
2020-08-11 14:50:59 +0700

    ddd

--- api/index.js
+++ api/index.js
@@ -54,7 +54,7 @@ function sendMessage(senderId, message) {
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {
-      access_token: "token",
+      access_token: "EAAsHu2amnGsBACaZAIUs2iHQ4WU8XX6Y3R4PsDnPj8YfKmY5tNK4zumZCprU2CbS7bjAZAtkIF2ZAvFS99vZCTbW3UeWnDpf50M5egBRnPUsOSvXYMIRZClEvdIk4ZC0hEo6QVQFwQ1H90OkvLQDfuEqMZAB36fVxtsPtbVZCAZACsZBQ5310fUzGpN",
     },
     method: 'POST',
     json: {
