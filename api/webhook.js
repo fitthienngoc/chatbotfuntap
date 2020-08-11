7182b6f7546cee5ab6cc6af25cdb4795ab2946d2
Thien Ngoc <ngocnt@funtap.vn>
2020-08-11 18:05:58 +0700

    dddx

--- api/webhook.js
+++ api/webhook.js
@@ -95,28 +95,27 @@ module.exports = (req, res) => {
 
 
 // Gửi thông tin tới REST API để trả lời
-const sendMessage = (senderId, message) => {
-  console.log(senderId, message);
-  request(
-    {
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {
-        access_token: "EAAsHu2amnGsBACaZAIUs2iHQ4WU8XX6Y3R4PsDnPj8YfKmY5tNK4zumZCprU2CbS7bjAZAtkIF2ZAvFS99vZCTbW3UeWnDpf50M5egBRnPUsOSvXYMIRZClEvdIk4ZC0hEo6QVQFwQ1H90OkvLQDfuEqMZAB36fVxtsPtbVZCAZACsZBQ5310fUzGpN",
-      },
-      method: 'POST',
-      json: {
-        recipient: {
-          id: senderId
+const sendMessage =  (senderId, message) => {
+  const send = async ()=>{
+     const req =  await request(
+      {
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+          access_token: "EAAsHu2amnGsBACaZAIUs2iHQ4WU8XX6Y3R4PsDnPj8YfKmY5tNK4zumZCprU2CbS7bjAZAtkIF2ZAvFS99vZCTbW3UeWnDpf50M5egBRnPUsOSvXYMIRZClEvdIk4ZC0hEo6QVQFwQ1H90OkvLQDfuEqMZAB36fVxtsPtbVZCAZACsZBQ5310fUzGpN",
         },
-        message: {
-          text: message
-        },
-      }
-    }, function (error, response, body) {
-      console.error('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body); // Print the HTML for the Google homepage.
-    })
+        method: 'POST',
+        json: {
+          recipient: {
+            id: senderId
+          },
+          message: {
+            text: message
+          },
+        }
+      })
+      console.log(req);
+  }
+  send();
 }
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
