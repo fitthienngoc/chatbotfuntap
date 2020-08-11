aa4c7048ee0507ee589401cef292dfd787356801
Thien Ngoc <ngocnt@funtap.vn>
2020-08-11 17:17:48 +0700

    dddx

--- api/webhook.js
+++ api/webhook.js
@@ -18,35 +18,54 @@ var request = require("request");
 
 
 // Xử lý khi có người nhắn tin cho bot
-app.post('/api/webhook', function (req, res) {
-  console.log('xxx',req);
-  console.log('xxx@@@',res);
-  //   const {body} = req
-  //   const {entry} = body
-  //   const {messaging} = entry
-  // console.log(JSON.stringify(body));
-  // console.log({req});
-  // console.log({res});
+module.exports = (req, res) => {
+  if (body)
+    let entries = req.body.entry;
+  for (let entry of entries) {
+    let messaging = entry.messaging;
+    for (let message of messaging) {
+      let senderId = message.sender.id;
+      if (message.message) {
+        // If user send text
+        if (message.message.text) {
+          let text = message.message.text;
+          sendMessage(senderId, "Tui là bot đây: " + text)
+        }
+      }
+    }
+  }
+  res.status(200).send("OK");
+}
 
-  // if(body)
-  // const {entry} = 
-  // let entries = req.body.entry;
-  // for (let entry of entries) {
-  //   let messaging = entry.messaging;
-  //   for (let message of messaging) {
-  //     let senderId = message.sender.id;
-  //     if (message.message) {
-  //       // If user send text
-  //       if (message.message.text) {
-  //         let text = message.message.text;
-  //         sendMessage(senderId, "Tui là bot đây: " + text)
-  //       }
-  //     }
-  //   }
-  // }
+// app.post('/api/webhook', function (req, res) {
+//   console.log('xxx', req);
+//   console.log('xxx@@@', res);
+//   //   const {body} = req
+//   //   const {entry} = body
+//   //   const {messaging} = entry
+//   // console.log(JSON.stringify(body));
+//   // console.log({req});
+//   // console.log({res});
 
-  // res.status(200).send("OK");
-});
+//   // if(body)
+//   // const {entry} = 
+//   // let entries = req.body.entry;
+//   // for (let entry of entries) {
+//   //   let messaging = entry.messaging;
+//   //   for (let message of messaging) {
+//   //     let senderId = message.sender.id;
+//   //     if (message.message) {
+//   //       // If user send text
+//   //       if (message.message.text) {
+//   //         let text = message.message.text;
+//   //         sendMessage(senderId, "Tui là bot đây: " + text)
+//   //       }
+//   //     }
+//   //   }
+//   // }
+
+//   // res.status(200).send("OK");
+// });
 
 
 // Gửi thông tin tới REST API để trả lời
@@ -66,7 +85,7 @@ const sendMessage = (senderId, message) => {
           text: message
         },
       }
-    },function (error, response, body) {
+    }, function (error, response, body) {
       console.error('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       console.log('body:', body); // Print the HTML for the Google homepage.
